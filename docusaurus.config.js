68e5a957f2e6e2273ca3149be919ec036af2e1f1
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2023-05-10 17:02:45 +0300

    [Fix] Remove unused static assets.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,8 +54,6 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
       docs: {
         sidebar: {
           hideable: false,
